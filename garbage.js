f3da5c497a15128b55bf5b1f58c1b9672918c8d0
Ramón Alberto Gómez Urquídez <gomez.ramon2004@gmail.com>
2023-10-07 08:55:04 -0700

    i put the garbage away from here >:C

--- garbage.js
+++
@@ -1,3 +0,0 @@
-promH.textContent = `Average Hue: ${huePerFrame.reduce((a, b) => a + b, 0) / huePerFrame.length}`;
-promS.textContent = `Average Saturation: ${saturationPerFrame.reduce((a, b) => a + b, 0) / saturationPerFrame.length}`;
-promB.textContent = `Average Brightness: ${brightnessPerFrame.reduce((a, b) => a + b, 0) / brightnessPerFrame.length}`;
